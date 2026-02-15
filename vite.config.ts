569dcca622481c0c43fc85e1661ca92d6d260f57
Amogha Skanda Vishwanath <amoghaskanda.vishwanath@mail.polimi.it>
2026-02-15 11:51:15 +0100

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked relatively, making it compatible with GitHub Pages repo paths
+  base: '/pharaohs-blocks/', // Ensures assets are linked relatively, making it compatible with GitHub Pages repo paths
 });
